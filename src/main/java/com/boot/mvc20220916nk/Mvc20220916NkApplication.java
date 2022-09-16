c7d1f19032483961fa5021353de0d1b0b59344b5
nage24 <d0190466@kowon.dongseo.ac.kr>
2022-09-16 09:20:48 +0900

    2022-09-16 springboot-mvc-20220916-nk project

---
+++ src/main/java/com/boot/mvc20220916nk/Mvc20220916NkApplication.java
@@ -0,0 +1,13 @@
+package com.boot.mvc20220916nk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220916NkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220916NkApplication.class, args);
+	}
+
+}
